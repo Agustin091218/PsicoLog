5da8b2d08b701ef230a776b20ab147a68411ab02
Axel Mrak <axelmrak@gmail.com>
2026-04-27 18:30:01 -0300

    add migration patient

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_04_23_160424) do
+ActiveRecord::Schema[7.2].define(version: 2026_04_27_211348) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "patients", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "user_id", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "deleted_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["deleted_at"], name: "index_patients_on_deleted_at"
+    t.index ["user_id"], name: "index_patients_on_user_id"
+  end
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "first_name", null: false
@@ -27,4 +38,6 @@ ActiveRecord::Schema[7.2].define(version: 2026_04_23_160424) do
     t.index ["deleted_at"], name: "index_users_on_deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
+  add_foreign_key "patients", "users"
 end
