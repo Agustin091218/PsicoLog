8dea7adf594ddda84fa127993d88e50818103de3
Axel Mrak <axelmrak@gmail.com>
2026-05-11 16:36:25 -0300

    load trix via script tag instead of importmap for reliable custom element registration

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,4 @@
 pin "application", preload: true
 pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.2.2/dist/stimulus.js"
 pin "@hotwired/stimulus-loading", to: "https://ga.jspm.io/npm:@hotwired/stimulus-loading@2.0.1/dist/stimulus-loading.js"
-pin "trix", to: "https://cdn.jsdelivr.net/npm/trix@2.1.0/dist/trix.umd.min.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
