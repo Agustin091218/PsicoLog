1a9f0748c12748a8208eafa052bc3bd165f06cb3
Axel Mrak <axelmrak@gmail.com>
2026-05-11 12:48:48 -0300

    add routes user and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :users, only: %i[new create]
+  resources :sessions, only: %i[new create destroy]
+  root "sessions#new"
 end
